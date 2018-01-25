f308ff18acbf9513f38b6289139778430ea610db
Admin <admin@Willis-MacBook-Pro-8.local>
2018-01-25 13:58:26 -0500

    Enemy now spawns in different spots

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,50 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 15.0f;
-	public float padding = .03f;
-	private float xmin;
-	private float xmax;
-	private float ymin = -4.5f;
-	private float ymax = -1.66f;
-	// Use this for initialization
-	void Start ()
-	{
-		float distance = transform.position.z - Camera.main.transform.position.z;
-		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
-		Vector3 Rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
-		xmin = leftmost.x + padding;
-		xmax = Rightmost.x - padding;
-	}
+    public float speed = 15.0f;
+    public float padding = .5f;
+    private float xmin;
+    private float xmax;
+    private float ymin = -4.5f;
+    private float ymax = -1.66f;
+    // Use this for initialization
+    void Start ()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+        Vector3 Rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1,0,distance));
+        xmin = leftmost.x + padding;
+        xmax = Rightmost.x - padding;
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
-			transform.position +=  Vector3.left * speed * Time.deltaTime ;
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position +=  Vector3.left * speed * Time.deltaTime ;
 			
-		}
-		else if (Input.GetKey(KeyCode.RightArrow))
-		{
-			transform.position += Vector3.right * speed * Time.deltaTime;
-		}
-		else if (Input.GetKey(KeyCode.UpArrow))
-		{
-			transform.position += Vector3.up * speed * Time.deltaTime;
-		}
-		else if (Input.GetKey(KeyCode.DownArrow))
-		{
-			transform.position += Vector3.down * speed * Time.deltaTime;
-		}
-		// restricts the player to the gamespace.
-		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
-		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
-		float newY = Mathf.Clamp(transform.position.y, ymin, ymax);
-		transform.position = new Vector3(transform.position.x, newY , transform.position.z);
-	}
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+        // restricts the player to the gamespace.
+        float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+        float newY = Mathf.Clamp(transform.position.y, ymin, ymax);
+        transform.position = new Vector3(transform.position.x, newY , transform.position.z);
+    }
 
-	void MoveWithArrow()
-	{
+    void MoveWithArrow()
+    {
 		
-	}
+    }
 }
---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+
+
+	
+	public GameObject enemyPrefab;
+		// Use this for initialization
+	void Start () {
+			foreach (Transform child in transform)
+			{
+				GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity) as GameObject;
+				enemy.transform.parent = child;
+			}
+		}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
